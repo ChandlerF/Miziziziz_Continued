e8101f9f3172c8ef79f0989a4afcdfff324845e0
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-08-24 21:05:55 -0500

    Small Changes

--- Assets/DifficultyManager.cs
+++ Assets/DifficultyManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DifficultyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ScoreM;
+    public GameObject[] Enemies;
+
+
+
     void Start()
     {
-        
+        ScoreM = GameObject.Find("ScoringManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float NewSpeed = ScoreM.GetComponent<ScoringManager>().Score / 15;
+       // foreach (Enemy in Enemies)
+        //{
+           
+        //}
     }
 }
+// = Enemies.GetComponent<EnemyMovement2>().Speed;

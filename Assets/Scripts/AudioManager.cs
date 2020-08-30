f65c93a8ad011725bed18447790eeb94b4ff7cdd
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-08-29 20:54:00 -0500

    Added A Mute Possibility

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,8 @@ public class AudioManager : MonoBehaviour
 
     public static AudioManager Instance;
 
+    public bool Muted = false;
+
     void Awake()
     {
         if(Instance == null)  //It makes it so where there can't be 2 AudioManagers in a Scene
@@ -32,6 +34,32 @@ public class AudioManager : MonoBehaviour
             s.Source.loop = s.Loop;
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && Muted == true)
+        {
+            Muted = false;
+            Debug.Log("Un-Mute");
+        } else if (Input.GetKeyDown(KeyCode.M) && Muted == false)
+        {
+            Muted = true;
+            Debug.Log("Mute");
+        }
+
+        foreach(Sound s in Sounds)
+        {
+            if (Muted == true)
+            {
+                s.Source.volume = 0f;
+            }
+            else
+            {
+                s.Source.volume = s.Volume;
+            }
+        }
+    }
+
 
     public void Play (string name)
     {

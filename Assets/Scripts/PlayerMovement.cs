b9c2526e50031b08c30921cdac6ac5d5fa72d6b9
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-09-02 20:48:17 -0500

    Implemented a good dash system and am trying to add upgrade screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,17 +12,41 @@ public class PlayerMovement : MonoBehaviour
 
 
     public CameraShake Camera_Shake;
+    public bool UnlockedDash = true;
+    public bool IsDashing = false;
+    public float DashSpeed;
+    public float DashDelay;
+    public float StartDashDelay;
+    public bool StartTheDashDelay = false;
 
+
+   
 
     void Update()  //Input
     {
         Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
+
+        if(StartTheDashDelay == true && DashDelay > 0)
+        {
+            DashDelay -= Time.deltaTime;
+        }
+
+        if (UnlockedDash == true && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(DashDelay <= 0)
+            {
+                StartCoroutine(Dash(.08f));
+            }
+        }
     }
 
     void FixedUpdate()  //Movement (physics)
     {
-        rb.MovePosition(rb.position + Movement * MoveSpeed * Time.fixedDeltaTime);
+        if (IsDashing == false)
+        {
+            rb.MovePosition(rb.position + Movement * MoveSpeed * Time.fixedDeltaTime);
+        }
     }
 
 
@@ -31,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
         if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "EnemyProjectile")
         {
             PlayerDeath();
-           
+
         }
     }
 
@@ -45,4 +69,25 @@ public class PlayerMovement : MonoBehaviour
         ScoreText2.fontSize = 100;
         Time.timeScale = 0;
     }
+
+
+    public IEnumerator Dash(float Duration)
+    {
+        double Elapsed = 0.0f;
+
+        while (Elapsed < Duration)
+        {
+            IsDashing = true;
+
+            transform.position = Vector2.MoveTowards(transform.position, (rb.position + Movement), DashSpeed * Time.deltaTime);
+
+            Elapsed += .01;
+
+            yield return null;
+        }
+        DashDelay = StartDashDelay;
+        StartTheDashDelay = true;
+        IsDashing = false;
+    }
 }
+//        rb.AddForce((rb.position + Movement) * .06f, ForceMode2D.Impulse);
---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public PlayerMovement Player;
+    public BowController Bow;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void IncreasePlayerSpeed()
+    {
+        Player.MoveSpeed += (Player.MoveSpeed / 5f);  //Speed goes from 5 to 6
+    }
+
+    public void IncreasePlayerFireRate()
+    {
+        Bow.ShootDelay -= (Bow.ShootDelay / 5f);  //Delay between shots goes from .3 to .24
+    }
+
+    public void UnlockDash()
+    {
+        Player.UnlockedDash = true;
+    }
+}

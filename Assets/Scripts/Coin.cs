d4b8bfc5e03f3c5493cd9ab17ad0dafa1befec4d
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-11-08 23:31:25 -0600

    Coin System

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    private CoinManager CoinManagerObject;
+
+   public void Start()
+    {
+
+        CoinManagerObject = GameObject.Find("CoinManager").GetComponent <CoinManager> ();
+        //CoinManagerObject = GameObject.Find("CoinManager").GetComponent("CoinManager");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<AudioManager>().Play("CoinPickup");
+        CoinManagerObject.AddCoinScore();
+        CoinManagerObject.SpawnCoin();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    public GameObject Coin;
+    private Vector2 SpawnLocation;
+
+    [SerializeField] TMPro.TextMeshProUGUI CoinScoreText;
+    private float CoinScore = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCoin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnCoin()
+    {
+        SpawnLocation.x = Random.Range(-14f, 14f);
+        SpawnLocation.y = Random.Range(-10f, 10f);
+
+        if(Vector2.Distance(Player.transform.position,  SpawnLocation) > 5)
+        {
+            Instantiate(Coin, SpawnLocation, Quaternion.identity);
+        }
+        else
+        {
+            SpawnCoin();
+        }
+    }
+
+    public void AddCoinScore()
+    {
+        CoinScore += 1;
+        CoinScoreText.text = CoinScore.ToString();
+    }
+}

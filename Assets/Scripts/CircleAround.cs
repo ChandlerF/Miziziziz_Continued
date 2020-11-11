517902dc278a8581d53ae071d5f17dc211af1386
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-11-10 22:43:41 -0600

    Indicator Works
    No clue how it's this simple, am just glad :')

--- Assets/Scripts/CircleAround.cs
+++ Assets/Scripts/CircleAround.cs
@@ -12,16 +12,10 @@ public class CircleAround : MonoBehaviour
     private void Update()
     {
 
-        if(Vector2.Distance(Player.transform.position, transform.position) < 2)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, Coin.transform.position, Speed * Time.deltaTime);
-            //transform.rotation = 
-        }
-
-        if(Vector2.Distance(Player.transform.position, transform.position) > 2.0001f)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime);
-        }
+        Vector3 vectorToTarget = Coin.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg -90f;
+        //Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
 
@@ -39,6 +33,33 @@ public class CircleAround : MonoBehaviour
 
 
 
+
+
+
+
+
+
+
+
+
+
+    /*if(Vector2.Distance(Player.transform.position, transform.position) < 1)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Coin.transform.position, Speed * Time.deltaTime);
+        //transform.rotation = 
+    }
+
+    if(Vector2.Distance(Player.transform.position, transform.position) > 1.0001f)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime);
+    }
+
+
+     Vector3 vectorToTarget = Coin.transform.position - transform.position;
+    float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg -90f;
+    Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+    transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * Speed);
+    */
 
 
 
